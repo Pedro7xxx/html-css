11934e9e940a58e0d32ae60874cb1db8a4019213
Pedro7xxx <apppedro204@gmail.com>
2024-08-15 18:13:14 -0300

    Create Variáveis, Tipos de Dados e Operadores.js

---
+++ FUNDAMENTOS DA PROGRAMAÇÃO WEB UNINTER/Aula05_JAVASCRIPT 1/Variáveis, Tipos de Dados e Operadores.js
@@ -0,0 +1,23 @@
+// Variaveis
+    //let e var
+
+//Constante
+    //const
+
+//Exemplo-Variaveis
+    // let nome="Pedro";
+    // console.log(nome);
+
+//Tipos de dados - Dados literais primitivos
+    //Boolean - true ou false
+    //Number - Números reais (decimais e inteiros)
+    //BigInt - números inteiros de qualquer tamanho
+    //String - representa uma sequência de caracterers
+    //Undefined - valor padrão de uma variável, sem valor atribuido
+    //Symbol - Simbolo
+
+//Interpolação de expressões
+    //let a=5;
+    //let b=10;
+    //console.log
+    //(`O resultado da operação é ${a+b}`);
