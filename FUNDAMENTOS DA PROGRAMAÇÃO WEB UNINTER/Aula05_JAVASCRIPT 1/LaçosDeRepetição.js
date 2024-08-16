e06dff9c204908bb08cf1a5e15037cb0cffbea3d
Pedro7xxx <apppedro204@gmail.com>
2024-08-16 17:56:40 -0300

    .

---
+++ FUNDAMENTOS DA PROGRAMAÇÃO WEB UNINTER/Aula05_JAVASCRIPT 1/LaçosDeRepetição.js
@@ -0,0 +1,12 @@
+//For
+    //Executamos o for em situações em que sabemos quantas vezes executar o loop
+//Sintaxe
+    // for (inicialização; condição; incremento){
+    //     bloco de código (faça isso)
+    // }
+
+//Exemplo
+    for(let i=0; i<10; i++){
+        console.log(i);
+    }
+    
