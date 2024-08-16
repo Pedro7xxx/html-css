cd279bff8befbf2141a1072393df694a5ae46957
Pedro7xxx <apppedro204@gmail.com>
2024-08-15 21:30:18 -0300

    Update Variáveis, Tipos de Dados e Operadores.js

--- FUNDAMENTOS DA PROGRAMAÇÃO WEB UNINTER/Aula05_JAVASCRIPT 1/Variáveis, Tipos de Dados e Operadores.js
+++ FUNDAMENTOS DA PROGRAMAÇÃO WEB UNINTER/Aula05_JAVASCRIPT 1/Variáveis, Tipos de Dados e Operadores.js
@@ -8,6 +8,9 @@
     // let nome="Pedro";
     // console.log(nome);
 
+//Variáveis locais e globais
+    //Se utilizarmos  let e console fora de blocos de código, que são delimitados por {}, serão variáveis globais
+
 //Tipos de dados - Dados literais primitivos
     //Boolean - true ou false
     //Number - Números reais (decimais e inteiros)
@@ -21,3 +24,14 @@
     //let b=10;
     //console.log
     //(`O resultado da operação é ${a+b}`);
+
+//Métodos
+    //length: propriedade que informa  o número de caracteres de uma string
+    //charAt(index): informa a posição de "index" da string (começa com zero)
+
+    //slice(beginIndex, [optional] endIndex)
+        //Método que retorna uma nova string que é criada a partir das caracteres entre beginIndex (incluído) e endIndex (excluído)
+    //Se endIndex for omitido, a nova string será de beginIndex até o final da string
+
+    //split(separator, [opcional] limit)
+        //Divide a string  em substrings sempre que um separador é encontrado nessa string e retorna um array dessas substrings, enquanto um limite opcional limit limita o número de substrings adicional à lista
