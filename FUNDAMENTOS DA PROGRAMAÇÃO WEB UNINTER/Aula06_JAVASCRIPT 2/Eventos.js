53d35623adaba4a2ddecf88171d6a7c9569a5b02
Pedro7xxx <apppedro204@gmail.com>
2024-08-19 21:41:18 -0400

    .

---
+++ FUNDAMENTOS DA PROGRAMAÇÃO WEB UNINTER/Aula06_JAVASCRIPT 2/Eventos.js
@@ -0,0 +1,12 @@
+//Eventos
+    // click - ocorre quando o usuário pressiona o botão esquerdo do mouse.
+    // dclick - Ocorre quando o usuário pressiona duas vezes o botão esquedo do mouse
+    // mousedown - Ocorre quando o usuário pressiona qualquer um dos botões do mouse
+    // mouseover - Ocorre quando o usuário solta qualquer um dos botões do mouse 
+    // mouseout - Ocorre quando o usuário desloca o ponteiro do mouse de onde estava
+    // mousemove - Ocorre quando o usuário move o ponteiro do mouse
+    // load -  Ocorre quando há o carregamento da página
+    // unload -  Ocorre quando há o fechamento de uma janela.
+    // scroll -  Ocorre quando o elemento possui barra de rolagem
+    // focus Ocorre quando o elemento recebe foco
+    
